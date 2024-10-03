c6903fbd62f2dc0269866176cb43b31ab04830f9
Richard Viann Morais <f20220229@dubai.bits-pilani.ac.in>
2024-10-03 14:42:25 +0400

    Create Register.vue

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Register</h2>
+    <form @submit.prevent="register">
+      <input v-model="username" placeholder="Username" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    async register() {
+      await axios.post('http://localhost:5000/register', {
+        username: this.username,
+        password: this.password
+      });
+      alert('Registered successfully!');
+    }
+  }
+};
+</script>
