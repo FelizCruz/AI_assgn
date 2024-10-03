092e804382e491e5fddbf2ddcee388dc02a86516
Richard Viann Morais <f20220229@dubai.bits-pilani.ac.in>
2024-10-03 14:42:50 +0400

    Create Login.vue

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="Username" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        await axios.post('http://localhost:5000/login', {
+          username: this.username,
+          password: this.password
+        });
+        alert('Login successful!');
+      } catch (error) {
+        alert('Invalid credentials!');
+      }
+    }
+  }
+};
+</script>
