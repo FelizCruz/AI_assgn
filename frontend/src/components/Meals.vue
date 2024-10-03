0c49b9737154a4f636e11eb02e5725df676c8cb8
Richard Viann Morais <f20220229@dubai.bits-pilani.ac.in>
2024-10-03 14:44:16 +0400

    Create Meals.vue

---
+++ frontend/src/components/Meals.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Meals</h2>
+    <ul>
+      <li v-for="meal in meals" :key="meal.id">{{ meal.name }} ({{ meal.type }})</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      meals: []
+    };
+  },
+  async created() {
+    const response = await axios.get('http://localhost:5000/meals');
+    this.meals = response.data;
+  }
+};
+</script>
